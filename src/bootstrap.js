df97b35c758cd100e6db0c63097697f2ed471a09
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-03 08:36:05 -0700

    inital commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import * as classChallenge from './classChallenge';
+
+console.log(classChallenge.default);
+console.log(classChallenge.sumOfArray([1,2,3]));
+console.log(classChallenge.welcome);
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function() {
+    return '<div>classChallenge</div>'
+}
+
+export const welcome = 'welcome';
+
+export function sumOfArray(arr) {
+    return arr.reduce((total, num) => { return total += num})
+}
